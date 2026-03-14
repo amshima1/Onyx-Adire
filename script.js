ed24c1535a179655686b118b54888396d0431f08
amshima1 <v.amshima9021@miva.edu.ng>
2026-03-14 23:00:05 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// DOM Manipulation: Changing the header based on time of day
-document.addEventListener("DOMContentLoaded", () => {
-    const welcomeMsg = document.getElementById("welcome-text");
-    const hour = new Date().getHours();
-    
-    if (hour < 12) welcomeMsg.textContent = "Good Morning, Welcome to Onyx & Adire";
-    else if (hour < 18) welcomeMsg.textContent = "Good Afternoon, Explore our Collections";
-    else welcomeMsg.textContent = "Good Evening, Shop the Night Edit";
-});
